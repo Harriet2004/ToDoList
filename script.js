77d776ed68616b5fbd7985939bb03e16d3af412f
Harriet2004 <s3933558@student.rmit.edu.au>
2024-07-19 18:43:28 +1000

    added some events

--- script.js
+++ script.js
@@ -4,5 +4,29 @@ let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoInput = document.getElementById("input");
 const todoList = document.getElementById("list");
 const todoCount = document.getElementById("count");
-const addTasks = document.querySelector(".btn");
+const addTask = document.querySelector(".btn");
 const deleteTask = document.getElementById("delete");
+
+document.addEventListener("DOMContentLoaded", function() {
+    addTask.addEventListener("click", addTask());
+    todoInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteTask.addEventListener("click", deleteTasks());
+    displayTasks();
+});
+
+function addTask() {
+
+}
+
+function deleteTasks() {
+
+}
+
+function displayTasks() {
+
+}
